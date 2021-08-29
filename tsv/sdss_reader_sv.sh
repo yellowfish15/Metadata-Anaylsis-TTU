4deee27b7280ffb476dd97f7ef83515ae7eb46bd
Yu Lim <61293215+yellowfish15@users.noreply.github.com>
2021-08-29 13:21:47 -0500

    Shell script to run on HPCC

---
+++ tsv/sdss_reader_sv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Read_HDF5_Files_To_SV_Python
+#SBATCH --output=%x.o%j
+#SBATCH --error=%x.e%j
+#SBATCH --partition nocona
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=48:00:00
+##SBATCH --mem-per-cpu=3994MB  ##3.9GB, Modify based on needs   
+
+. $HOME/conda/etc/profile.d/conda.sh
+conda activate
+
+conda install mpi4py
+conda install numpy
+conda install h5py
+
+python /home/lim11950/sdss_reader_sv_mpi.py -d /home/lim11950/spectro/redux/103
